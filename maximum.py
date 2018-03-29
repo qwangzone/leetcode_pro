3790e7f7cbcbef0072d52625b598132866a2f27a
qwangzone <578380132@qq.com>
2018-03-29 18:00:27 +0800

    submit

---
+++ maximum.py
@@ -0,0 +1,17 @@
+class Solution:
+    # @param A, a list of integers
+    # @return an integer
+    # 6:57
+    def maxSubArray(self, A):
+        if not A:
+            return 0
+
+        curSum = maxSum = A[0]
+        for num in A[1:]:
+            curSum = max(num, curSum + num)
+            maxSum = max(maxSum, curSum)
+
+        print(maxSum)
+
+a = Solution()
+a.maxSubArray([1, 0, 1,1,1,1])
