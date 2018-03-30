d0e7b9983e227492bfafae56ae6f8a593d4cc1d2
qwangzone <578380132@qq.com>
2018-03-30 09:26:21 +0800

    submit

--- maximum.py
+++ maximum.py
@@ -11,7 +11,7 @@ class Solution:
             curSum = max(num, curSum + num)
             maxSum = max(maxSum, curSum)
 
-        print(maxSum)
+        return maxSum
 
 a = Solution()
 a.maxSubArray([1, 0, 1,1,1,1])
